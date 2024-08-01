d6b3abf2702ee0addc5207c1253b44bbba3080d5
mahyab <mahyab79@gmail.com>
2024-08-01 22:55:04 +0330

    add delete snippet action to component

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { SnippetShowPageProps } from "./snippetShowPageProps.type";
 import { notFound } from "next/navigation";
 import { db } from "@/db";
+import * as actions from "@/actions";
 import Link from "next/link";
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
@@ -10,13 +11,21 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
+  const deleteSnippetAction = actions.deleteSnippet.bind(null , snippet.id)
   return (
     <div>
       <div className="flex m-4 justify-between items-center ">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
-          <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-          <button className="p-2 border rounded">Delete</button>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-300 border-gray-200">
