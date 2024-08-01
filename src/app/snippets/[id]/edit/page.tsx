5b4343c1dc4b928e461af7d573d50294dcb741ed
mahyab <mahyab79@gmail.com>
2024-08-01 22:54:43 +0330

    add delete snippet action

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,13 +1,17 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
 import { SnippetShowPageProps } from "../snippetShowPageProps.type";
+import SnippetEditForm from "@/components/SnippetEditForm";
+
 export default async function SnippetEditPage(props: SnippetShowPageProps) {
   const id = parseInt(props.params.id);
-    const snippet = await db.snippet.findFirst({
-        where:{id:id}
-    })
-    if(!snippet) {
-        return notFound()
-    }
-  return <div>{snippet.title}</div>;
+  const snippet = await db.snippet.findFirst({
+    where: { id: id },
+  });
+  if (!snippet) {
+    return notFound();
+  }
+  return <div>
+    <SnippetEditForm snippet={snippet}/> 
+  </div>;
 }
