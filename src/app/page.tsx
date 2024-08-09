ba68f5365795f7bd72b3426b770dc30e235a1db9
mahyab <mahyab79@gmail.com>
2024-08-09 16:46:02 +0330

    practising on caching (on-demand and based-on-time)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import Link from "next/link";
+
 export default async function Home() {
   const snippets = await db.snippet.findMany();
   const renderedSnoppets = snippets.map((snippet) => {
@@ -18,7 +19,9 @@ export default async function Home() {
     <div>
       <div className="flex m-2 justify-between items-center">
         <h1 className="text-xl font-bold"></h1>
-        <Link href='/snippets/new' className="border p-2 rounded">New</Link>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
       </div>
       <div className="flex flex-col gap-2">{renderedSnoppets}</div>
     </div>
